b1c2ec5587e0643091f3179f0b3b3eabab35c492
Reto Gmür <reto@apache.org>
2014-10-14 12:39:01 +0200

    created project skeleton structure, no tests yet.

---
+++ src/main/java/eu/fusepool/geo/enriching/GeoEnrichingTransformer.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 Bern University of Applied Sciences.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.fusepool.geo.enriching;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.activation.MimeType;
+import javax.activation.MimeTypeParseException;
+
+import org.apache.clerezza.rdf.core.TripleCollection;
+import org.apache.clerezza.rdf.core.serializedform.Parser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.fusepool.p3.transformer.HttpRequestEntity;
+import eu.fusepool.p3.transformer.RdfGeneratingTransformer;
+
+public class GeoEnrichingTransformer extends RdfGeneratingTransformer {
+
+
+    private static final Logger log = LoggerFactory.getLogger(GeoEnrichingTransformer.class);
+
+    public GeoEnrichingTransformer() {
+    }
+
+    @Override
+    public Set<MimeType> getSupportedInputFormats() {
+        Parser parser = Parser.getInstance();
+        try {
+            Set<MimeType> mimeSet = new HashSet<MimeType>();
+            for (String mediaFormat : parser.getSupportedFormats()) {           
+              mimeSet.add(new MimeType(mediaFormat));
+            }
+            return Collections.unmodifiableSet(mimeSet);
+        } catch (MimeTypeParseException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    protected TripleCollection generateRdf(HttpRequestEntity entity) throws IOException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+  
+    @Override
+    public boolean isLongRunning() {
+        // downloading the dataset can be time consuming
+        return true;
+    }
+
+}
